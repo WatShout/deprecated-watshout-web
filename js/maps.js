15bd2a859deab9dd6fb59fa57021182b2fef5098
Ethan Houston <ethan.houston@gmail.com>
2018-04-21 21:15:21 -0700

    Polylines work

--- js/maps.js
+++ js/maps.js
@@ -8,12 +8,19 @@ let initMap = () => {
 
     let uluru = {lat: orgLat, lng: orgLong};
 
+    // flightPath is a SINGLE polyline
+    var flightPath;
+
+    // flightPathArray is an ARRAY of polylines
+    var flightPathArray = [];
+
     const map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 1,
+        zoom: 14,
         center: uluru
     });
 
     let markers = [];
+    let markerPos = [];
 
     ref.on(`child_added`, function (snapshot) {
 
@@ -25,6 +32,7 @@ let initMap = () => {
 
         let currentLocation = {lat: lat, lng: long};
 
+        // Center the map on current location
         map.panTo(currentLocation);
 
         addMarker(lat, long, time);
@@ -40,19 +48,36 @@ let initMap = () => {
 
         var image;
 
+        let currentCoords = new google.maps.LatLng(lat, long);
+
         let marker = new google.maps.Marker({
-            position: new google.maps.LatLng(lat, long),
+            position: currentCoords,
             map: map,
             icon: current
         });
 
+        markerPos.push(currentCoords);
+
         markers.push(marker);
+
+        flightPath = new google.maps.Polyline({
+          path: markerPos,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+        flightPath.setMap(map);
+
+        flightPathArray.push(flightPath);
 
         if (markers.length > 0){
 
             for (let i = 0; i < markers.length - 1; i++){
 
-                markers[i].setIcon(beachFlag);
+                markers[i].setIcon(null);
+                markers[i].setVisible(false);
 
             }
         }
@@ -68,7 +93,16 @@ let initMap = () => {
 
     // Removes the markers from the map, but keeps them in the array.
     function clearMarkers() {
+
         setMapOnAll(null);
+        flightPath.setMap(null);
+
+        for (let i=0; i < flightPathArray.length; i++){
+              flightPathArray[i].setMap(null); //or line[i].setVisible(false);
+        }
+
+        flightPathArray = [];
+        markerPos = [];
     }
 
     ref.on(`child_removed`, function (snapshot) {
