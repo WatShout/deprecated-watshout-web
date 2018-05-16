3d63aca6e501ea65c6391a7b434b4cd312d59849
Ethan Houston <ethan.houston@gmail.com>
2018-05-15 18:02:17 -0700

    Fixed some Javascript scope issues

--- public/js/labs.js
+++ public/js/labs.js
@@ -1,16 +1,3 @@
-// Initialize Firebase
-const config = {
-    apiKey: "AIzaSyDR-NpXGPfiFlm9Tr9u9_eS-0D0xt_3JDs",
-    authDomain: "gps-app-c31df.firebaseapp.com",
-    databaseURL: "https://gps-app-c31df.firebaseio.com",
-    projectId: "gps-app-c31df",
-    storageBucket: "",
-    messagingSenderId: "675799163706"
-};
-firebase.initializeApp(config);
-
-const database = firebase.database();
-
 let deleteMessages = () => {
 
     let ref = database.ref();
