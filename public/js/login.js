ec1748e50756b8f4dbc531dbffa58fc5f46d29f4
Ethan Houston <ethan.houston@gmail.com>
2018-05-16 17:16:31 -0700

    Seperated signup and login pages

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,7 @@
 
-document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
-
-let redirectURL = `../`;
 // FirebaseUI config.
 var uiConfig = {
-    signInSuccessUrl: redirectURL,
+    signInSuccessUrl: `../`,
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -19,37 +16,6 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
 
-function handleSignUp() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    if (email.length < 4) {
-        alert('Please enter an email address.');
-        return;
-    }
-    if (password.length < 4) {
-        alert('Please enter a password.');
-        return;
-    }
-    // Sign in with email and pass.
-    // [START createwithemail]
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode == 'auth/weak-password') {
-            alert('The password is too weak.');
-        } else {
-            alert(errorMessage);
-        }
-        console.log(error);
-        // [END_EXCLUDE]
-
-        window.location.replace(redirectURL);
-    });
-    // [END createwithemail]
-}
-
 function initApp() {
 
     // Listening for auth state changes.
@@ -59,7 +25,7 @@ function initApp() {
         // [END_EXCLUDE]
         if (user) {
             // User is signed in
-            window.location.replace(redirectURL);
+            window.location.replace(`../`);
         }
     });
 }
